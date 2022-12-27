0dd955587240a8212ad607f052c43908ea4ec489
Okuzawa <52669374+Okuzawa@users.noreply.github.com>
2022-12-28 01:52:23 +0900

    Create Title.js

---
+++ Title.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+  const canvas001 = document.getElementById("canvas001");
+    if (canvas001.getContext) {
+      const context001 = canvas001.getContext("2d");//2次元描画
+    }
+};

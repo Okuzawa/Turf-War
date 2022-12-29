2cfa409df74b4525eeb0479b83f9e22c864dbfc4
Okuzawa <52669374+Okuzawa@users.noreply.github.com>
2022-12-29 13:19:28 +0900

    改良

--- test/test_webStorage.js
+++ test/test_webStorage.js
@@ -1,33 +1,25 @@
 function userNameSet(){
-  let test_key = nameText.value;
-  if(localStorage.hasOwnProperty(test_key)) {
-    msg.innerText = test_key + 'は既に使われているよ';
+  let key = nameText.value;
+  if(localStorage.hasOwnProperty(key)) {
+    msg.innerText = key + 'は既に使われているよ';
   }
   else {
     let value = localStorage.length;
-    localStorage.setItem('test_key', 'value');
-    msg.innerText = test_key + 'さんのidを' + value + 'で登録したよ';
+    localStorage.setItem(test_key, value);
+    msg.innerText = key + 'さんのidを' + value + 'で登録したよ';
   }
 }
 
 function userNameGet(){
-  let test_key = nameText.value;
-  if(localStorage.hasOwnProperty(test_key)) {
-    let value = localStorage.getItem(test_key);
-    console.log(value)
-    msg.innerText = test_key + 'さんのidは' + String(value) + 'だよ';
+  let key = nameText.value;
+  if(localStorage.hasOwnProperty(key)) {
+    let value = localStorage.getItem(key);
+    msg.innerText = key + 'さんのidは' + value + 'だよ';
   }
   else {
-    msg.innerText = test_key + 'は存在しないアカウントだよ';
-  }
-
-  for (var key in localStorage) {
-    if (localStorage.hasOwnProperty(key)) {
-        console.log(key);
-    }
+    msg.innerText = key + 'は存在しないアカウントだよ';
   }
 }
-
 
 let nameText = document.getElementById('nameText');
 let msg = document.getElementById('msg');
